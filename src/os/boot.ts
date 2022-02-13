28159e994a95b02de7b178d7dfd1c4d0af2edacc
Rui Lopes <ruidlopes@gmail.com>
2022-02-12 20:47:26 -0500

    os: set rsp to stack offset defined by the program's header.
    this is done at boot time.

--- src/os/boot.ts
+++ src/os/boot.ts
@@ -1,6 +1,6 @@
 import {assemble} from '../asm';
 import {LinkerOptions} from '../asm/codegen';
-import {CODE_OFFSET, HEADER_OFFSET, MEMORY_PROGRAM_OFFSET, START_ADDRESS_OFFSET, VERSION_0_1} from '../core';
+import {CODE_OFFSET, HEADER_OFFSET, MEMORY_PROGRAM_OFFSET, STACK_ADDRESS_OFFSET, START_ADDRESS_OFFSET, VERSION_0_1} from '../core';
 
 const LINKER_OPTIONS: LinkerOptions = {
   header: false,
@@ -12,10 +12,16 @@ export function create_boot(): Uint8Array {
   // FIXME: move address computation to boot code itself
   return assemble(`
     MOV R0, ${MEMORY_PROGRAM_OFFSET + HEADER_OFFSET + CODE_OFFSET}
-    LOD R1, ${MEMORY_PROGRAM_OFFSET + HEADER_OFFSET + START_ADDRESS_OFFSET}
+    
+    // prepare stack
+    LOD R1, ${MEMORY_PROGRAM_OFFSET + HEADER_OFFSET + STACK_ADDRESS_OFFSET}
+    ADD R1, R0
+    MOV RSP, R1
 
-    ADD R0, R1
+    // prepare to jump to start address
+    LOD R2, ${MEMORY_PROGRAM_OFFSET + HEADER_OFFSET + START_ADDRESS_OFFSET}
+    ADD R2, R0
 
-    JMP R0
+    JMP R2
   `, LINKER_OPTIONS);
 }
